77340ec207303cb9334ed6469ac6b0a914a13c4c
mjain5725 <jain11y@uwindsor.ca>
2021-05-22 13:42:34 -0400

    Update password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,11 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword 
+);
 
 router
   .route('/')
