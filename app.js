209c13070cf40e836595d892b53f5990f9b88990
mjain5725 <jain11y@uwindsor.ca>
2021-05-24 23:27:38 -0400

    bug fixes and minor changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -39,6 +40,20 @@ app.use(mongoSanitize());
 
 // Data Sanitization against NOSQL XSS attacks(X site scripting)
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Serving static file
 app.use(express.static(`${__dirname}/public`));
