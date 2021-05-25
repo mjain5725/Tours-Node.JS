a99634b87c1c54ac31a69bcd8dd7c5f5b2a90554
mjain5725 <jain11y@uwindsor.ca>
2021-05-24 23:13:43 -0400

    security implementation

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 /* eslint-disable prettier/prettier */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,15 +13,37 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MiddleWares
+// 1) Global MiddleWares
+// Set Security for the HTTP Headers
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Limit the requests from same api
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP please try again in an hour!',
+});
 
+app.use('/api', limiter);
+
+// Body Parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NOSQL query  injection
+app.use(mongoSanitize());
+
+// Data Sanitization against NOSQL XSS attacks(X site scripting)
+app.use(xss());
+
+// Serving static file
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
